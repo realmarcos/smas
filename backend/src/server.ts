54ea1f1b9898f005355e0c2e503fc3630cca02fc
Marcos <realmarcos.raimundo@gmail.com>
2023-03-30 15:21:27 -0300

    ✨ Feat: add AppError for return errors message

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,5 +3,5 @@ import "./bootstrap";
 const app = express();
 
 app.listen(process.env.PORT, () => {
-	console.log(`Server started on port: ${process.env.PORT}`);
+  console.log(`Server started on port: ${process.env.PORT}`);
 });
