fd2adf761c6f4005d7cdd60eabcc6c2e5ef30323
Marcos <realmarcos.raimundo@gmail.com>
2023-03-30 12:05:23 -0300

    🔧 Config: added eslint settings

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,7 @@
-import express from 'express';
-import './bootstrap';
+import express from "express";
+import "./bootstrap";
 const app = express();
 
-const server = app.listen(process.env.PORT, () => {
-  console.log(`Server started on port: ${process.env.PORT}`);
+app.listen(process.env.PORT, () => {
+	console.log(`Server started on port: ${process.env.PORT}`);
 });
